1d718787088b90b0c4312f221857dc22441ffe20
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-20 21:52:17 +0700

    refactor: maintainer required

--- test/yarn-theme-loader.test.js
+++ test/yarn-theme-loader.test.js
@@ -1,28 +1,38 @@
 'use strict';
 
-// npx c8 --reporter=lcovonly mocha --timeout=70000 test/yarn-theme-loader.test.js
+// npx c8 --reporter=lcovonly mocha --timeout=7000000 test/yarn-theme-loader.test.js
 
+const { join, resolve, dirname } = require('path');
+const { readFileSync, writeFile, existsSync } = require('hexo-fs');
 const { spawn } = require('hexo-util');
-const path = require('path');
 const expect = require('chai').expect;
+const yaml = require('js-yaml');
+const testBase = join(__dirname, 'fixtures/yarn-workspace/site'); // join(__dirname, '../../../site'); //
+const configFile = join(testBase, '_config.yml');
+process.cwd = () => testBase;
 
-describe('Theme Yarn Workspace', () => {
+describe('find theme from yarn workspace', () => {
   const Hexo = require('../lib/hexo');
-  const base = path.normalize(__dirname, 'fixtures/yarn-workspace');
-  const hexo = new Hexo(base, { silent: true });
+  const cfg = yaml.load(readFileSync(configFile).toString());
+  const hexo = new Hexo(testBase, Object.assign(cfg, { silent: true }));
 
   before(async () => {
-    await spawn('yarn', ['install'], { cwd: base });
+    console.log('installing workspace');
+    if (!existsSync(join(dirname(testBase), 'yarn.lock'))) {
+      await writeFile(join(dirname(testBase), 'yarn.lock'), '');
+    }
+    await spawn('yarn', ['install'], { cwd: testBase });
+    console.log('initializing hexo');
     await hexo.init();
+    // await hexo.load();
+  });
+
+  it('base must equal', async () => {
+    expect(resolve(hexo.base_dir)).to.be.equal(resolve(testBase));
   });
 
   it('find theme from yarn workspace', async () => {
-    await hexo.init();
-    // const { theme_dir, base_dir, config, config_path } = hexo;
-    // console.log({ theme_dir, base_dir, config_path, theme: config.theme });
-    // path.normalize(base_dir).should.eql(base);
-    // check base is same
-    expect(path.resolve(hexo.base_dir)).to.be.equal(path.resolve(base));
-    // config.theme.should.eql('test');
+    console.log({ base: testBase.replace(__dirname, ''), theme: hexo.theme_dir.replace(__dirname, '') });
+    // expect(hexo.theme_dir).to.be
   });
 });
