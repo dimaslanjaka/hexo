2a13461903acd89661d21203de2ddf63481f3a44
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-23 17:18:12 +0700

    refactor: separate tests

--- test/post-asset-link.build.js
+++ test/post-asset-link.build.js
@@ -4,7 +4,5 @@ const Hexo = require("../packages/hexo/dist/hexo");
 
 const hexo = new Hexo(process.cwd(), { debug: true });
 hexo.init().then(function () {
-	hexo.log.i("base directory", hexo.base_dir);
-	hexo.log.i("theme directory", hexo.theme_dir);
-	hexo.log.i("theme script directory", hexo.theme_script_dir);
+	hexo.post.render("source/_posts/post-asset-link.md");
 });
