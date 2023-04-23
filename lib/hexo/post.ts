a0556dac9d0870e70b7320dc9e5c212528f706f3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-23 13:51:07 +0700

    chore: fix casting types

--- lib/hexo/post.ts
+++ lib/hexo/post.ts
@@ -5,11 +5,25 @@ import { join, extname, basename } from 'path';
 import { magenta } from 'picocolors';
 import { load } from 'js-yaml';
 import { slugize, escapeRegExp } from 'hexo-util';
-import { copyDir, exists, listDir, mkdirs, readFile, rmdir, unlink, writeFile } from 'hexo-fs';
-import { parse as yfmParse, split as yfmSplit, stringify as yfmStringify } from 'hexo-front-matter';
+import {
+  copyDir,
+  exists,
+  listDir,
+  mkdirs,
+  readFile,
+  rmdir,
+  unlink,
+  writeFile
+} from 'hexo-fs';
+import {
+  parse as yfmParse,
+  split as yfmSplit,
+  stringify as yfmStringify
+} from 'hexo-front-matter';
 const preservedKeys = ['title', 'slug', 'path', 'layout', 'date', 'content'];
 
-const rHexoPostRenderEscape = /<hexoPostRenderCodeBlock>([\s\S]+?)<\/hexoPostRenderCodeBlock>/g;
+const rHexoPostRenderEscape
+  = /<hexoPostRenderCodeBlock>([\s\S]+?)<\/hexoPostRenderCodeBlock>/g;
 
 const rSwigPlaceHolder = /(?:<|&lt;)!--swig\uFFFC(\d+)--(?:>|&gt;)/g;
 const rCodeBlockPlaceHolder = /(?:<|&lt;)!--code\uFFFC(\d+)--(?:>|&gt;)/g;
@@ -20,7 +34,8 @@ const STATE_SWIG_COMMENT = Symbol('swig_comment');
 const STATE_SWIG_TAG = Symbol('swig_tag');
 const STATE_SWIG_FULL_TAG = Symbol('swig_full_tag');
 
-const isNonWhiteSpaceChar = char => char !== '\r'
+const isNonWhiteSpaceChar = (char: string) =>
+  char !== '\r'
   && char !== '\n'
   && char !== '\t'
   && char !== '\f'
@@ -35,7 +50,7 @@ class PostRenderEscape {
     this.stored = [];
   }
 
-  static escapeContent(cache, flag, str) {
+  static escapeContent(cache: any[], flag: string, str: string) {
     return `<!--${flag}\uFFFC${cache.push(str) - 1}-->`;
   }
 
@@ -48,24 +63,32 @@ class PostRenderEscape {
     };
   }
 
-  restoreAllSwigTags(str) {
-    const restored = str.replace(rSwigPlaceHolder, PostRenderEscape.restoreContent(this.stored));
+  restoreAllSwigTags(str: string) {
+    const restored = str.replace(
+      rSwigPlaceHolder,
+      PostRenderEscape.restoreContent(this.stored)
+    );
     return restored;
   }
 
-  restoreCodeBlocks(str) {
-    return str.replace(rCodeBlockPlaceHolder, PostRenderEscape.restoreContent(this.stored));
+  restoreCodeBlocks(str: string) {
+    return str.replace(
+      rCodeBlockPlaceHolder,
+      PostRenderEscape.restoreContent(this.stored)
+    );
   }
 
-  escapeCodeBlocks(str) {
-    return str.replace(rHexoPostRenderEscape, (_, content) => PostRenderEscape.escapeContent(this.stored, 'code', content));
+  escapeCodeBlocks(str: string) {
+    return str.replace(rHexoPostRenderEscape, (_, content) =>
+      PostRenderEscape.escapeContent(this.stored, 'code', content)
+    );
   }
 
   /**
    * @param {string} str
    * @returns string
    */
-  escapeAllSwigTags(str) {
+  escapeAllSwigTags(str: string) {
     let state = STATE_PLAINTEXT;
     let buffer = '';
     let output = '';
@@ -81,7 +104,8 @@ class PostRenderEscape {
       const char = str[idx];
       const next_char = str[idx + 1];
 
-      if (state === STATE_PLAINTEXT) { // From plain text to swig
+      if (state === STATE_PLAINTEXT) {
+        // From plain text to swig
         if (char === '{') {
           if (next_char === '{') {
             state = STATE_SWIG_VAR;
@@ -103,14 +127,19 @@ class PostRenderEscape {
           output += char;
         }
       } else if (state === STATE_SWIG_TAG) {
-        if (char === '%' && next_char === '}') { // From swig back to plain text
+        if (char === '%' && next_char === '}') {
+          // From swig back to plain text
           idx++;
           if (swig_tag_name !== '' && str.includes(`end${swig_tag_name}`)) {
             state = STATE_SWIG_FULL_TAG;
           } else {
             swig_tag_name = '';
             state = STATE_PLAINTEXT;
-            output += PostRenderEscape.escapeContent(this.stored, 'swig', `{%${buffer}%}`);
+            output += PostRenderEscape.escapeContent(
+              this.stored,
+              'swig',
+              `{%${buffer}%}`
+            );
           }
 
           buffer = '';
@@ -137,12 +166,17 @@ class PostRenderEscape {
         if (char === '}' && next_char === '}') {
           idx++;
           state = STATE_PLAINTEXT;
-          output += PostRenderEscape.escapeContent(this.stored, 'swig', `{{${buffer}}}`);
+          output += PostRenderEscape.escapeContent(
+            this.stored,
+            'swig',
+            `{{${buffer}}}`
+          );
           buffer = '';
         } else {
           buffer = buffer + char;
         }
-      } else if (state === STATE_SWIG_COMMENT) { // From swig back to plain text
+      } else if (state === STATE_SWIG_COMMENT) {
+        // From swig back to plain text
         if (char === '#' && next_char === '}') {
           idx++;
           state = STATE_PLAINTEXT;
@@ -167,7 +201,11 @@ class PostRenderEscape {
 
           if (swig_full_tag_end_buffer.includes(`end${swig_tag_name}`)) {
             state = STATE_PLAINTEXT;
-            output += PostRenderEscape.escapeContent(this.stored, 'swig', `{%${swig_full_tag_start_buffer}%}${buffer}{%${swig_full_tag_end_buffer}%}`);
+            output += PostRenderEscape.escapeContent(
+              this.stored,
+              'swig',
+              `{%${swig_full_tag_start_buffer}%}${buffer}{%${swig_full_tag_end_buffer}%}`
+            );
             idx = _idx;
             swig_full_tag_start_buffer = '';
             swig_full_tag_end_buffer = '';
@@ -192,21 +230,31 @@ const prepareFrontMatter = (data, jsonMode) => {
     } else if (moment.isDate(item)) {
       data[key] = moment.utc(item).format('YYYY-MM-DD HH:mm:ss');
     } else if (typeof item === 'string') {
-      if (jsonMode || item.includes(':') || item.startsWith('#') || item.startsWith('!!')
-      || item.includes('{') || item.includes('}') || item.includes('[') || item.includes(']')
-      || item.includes('\'') || item.includes('"')) data[key] = `"${item.replace(/"/g, '\\"')}"`;
+      if (
+        jsonMode
+        || item.includes(':')
+        || item.startsWith('#')
+        || item.startsWith('!!')
+        || item.includes('{')
+        || item.includes('}')
+        || item.includes('[')
+        || item.includes(']')
+        || item.includes('\'')
+        || item.includes('"')
+      ) {
+        data[key] = `"${item.replace(/"/g, '\\"')}"`;
+      }
     }
   }
 
   return data;
 };
 
-
-const removeExtname = str => {
+const removeExtname = (str: string) => {
   return str.substring(0, str.length - extname(str).length);
 };
 
-const createAssetFolder = (path, assetFolder) => {
+const createAssetFolder = (path: string, assetFolder: boolean) => {
   if (!assetFolder) return Promise.resolve();
 
   const target = removeExtname(path);
@@ -237,11 +285,21 @@ class Post {
   public tag: any;
   public separator: any;
 
-  constructor(context) {
+  constructor(context: import('.')) {
     this.context = context;
   }
 
-  create(data, replace, callback?) {
+  create(
+    data: {
+      [key: string]: any;
+      slug?: string;
+      title?: string;
+      layout?: string;
+      date?: any;
+    },
+    replace: boolean,
+    callback?: (...args: any[]) => any
+  ) {
     if (!callback && typeof replace === 'function') {
       callback = replace;
       replace = false;
@@ -250,7 +308,9 @@ class Post {
     const ctx = this.context;
     const { config } = ctx;
 
-    data.slug = slugize((data.slug || data.title).toString(), { transform: config.filename_case });
+    data.slug = slugize((data.slug || data.title).toString(), {
+      transform: config.filename_case
+    });
     data.layout = (data.layout || config.default_layout).toLowerCase();
     data.date = data.date ? moment(data.date) : moment();
 
@@ -261,19 +321,21 @@ class Post {
         context: ctx
       }),
       this._renderScaffold(data)
-    ]).spread((path, content) => {
-      const result = { path, content };
+    ])
+      .spread((path, content) => {
+        const result = { path, content };
 
-      return Promise.all<void, void | string>([
-        // Write content to file
-        writeFile(path, content),
-        // Create asset folder
-        createAssetFolder(path, config.post_asset_folder)
-      ]).then(() => {
-        ctx.emit('new', result);
-        return result;
-      });
-    }).asCallback(callback);
+        return Promise.all<void, void | string>([
+          // Write content to file
+          writeFile(path, content),
+          // Create asset folder
+          createAssetFolder(path, config.post_asset_folder)
+        ]).then(() => {
+          ctx.emit('new', result);
+          return result;
+        });
+      })
+      .asCallback(callback);
   }
 
   _getScaffold(layout) {
@@ -289,42 +351,46 @@ class Post {
     const { tag } = this.context.extend;
     let splited;
 
-    return this._getScaffold(data.layout).then(scaffold => {
-      splited = yfmSplit(scaffold);
-      const jsonMode = splited.separator.startsWith(';');
-      const frontMatter = prepareFrontMatter({ ...data }, jsonMode);
+    return this._getScaffold(data.layout)
+      .then(scaffold => {
+        splited = yfmSplit(scaffold);
+        const jsonMode = splited.separator.startsWith(';');
+        const frontMatter = prepareFrontMatter({ ...data }, jsonMode);
 
-      return tag.render(splited.data, frontMatter);
-    }).then(frontMatter => {
-      const { separator } = splited;
-      const jsonMode = separator.startsWith(';');
+        return tag.render(splited.data, frontMatter);
+      })
+      .then(frontMatter => {
+        const { separator } = splited;
+        const jsonMode = separator.startsWith(';');
 
-      // Parse front-matter
-      const obj = jsonMode ? JSON.parse(`{${frontMatter}}`) : load(frontMatter);
+        // Parse front-matter
+        const obj = jsonMode
+          ? JSON.parse(`{${frontMatter}}`)
+          : load(frontMatter);
 
-      Object.keys(data)
-        .filter(key => !preservedKeys.includes(key) && obj[key] == null)
-        .forEach(key => {
-          obj[key] = data[key];
+        Object.keys(data)
+          .filter(key => !preservedKeys.includes(key) && obj[key] == null)
+          .forEach(key => {
+            obj[key] = data[key];
+          });
+
+        let content = '';
+        // Prepend the separator
+        if (splited.prefixSeparator) content += `${separator}\n`;
+
+        content += yfmStringify(obj, {
+          mode: jsonMode ? 'json' : ''
         });
 
-      let content = '';
-      // Prepend the separator
-      if (splited.prefixSeparator) content += `${separator}\n`;
+        // Concat content
+        content += splited.content;
 
-      content += yfmStringify(obj, {
-        mode: jsonMode ? 'json' : ''
+        if (data.content) {
+          content += `\n${data.content}`;
+        }
+
+        return content;
       });
-
-      // Concat content
-      content += splited.content;
-
-      if (data.content) {
-        content += `\n${data.content}`;
-      }
-
-      return content;
-    });
   }
 
   publish(data, replace, callback) {
@@ -338,7 +404,9 @@ class Post {
     const ctx = this.context;
     const { config } = ctx;
     const draftDir = join(ctx.source_dir, '_drafts');
-    const slug = slugize(data.slug.toString(), { transform: config.filename_case });
+    const slug = slugize(data.slug.toString(), {
+      transform: config.filename_case
+    });
     data.slug = slug;
     const regex = new RegExp(`^${escapeRegExp(slug)}(?:[^\\/\\\\]+)`);
     let src = '';
@@ -347,37 +415,44 @@ class Post {
     data.layout = (data.layout || config.default_layout).toLowerCase();
 
     // Find the draft
-    return listDir(draftDir).then(list => {
-      const item = list.find(item => regex.test(item));
-      if (!item) throw new Error(`Draft "${slug}" does not exist.`);
+    return listDir(draftDir)
+      .then(list => {
+        const item = list.find(item => regex.test(item));
+        if (!item) throw new Error(`Draft "${slug}" does not exist.`);
 
-      // Read the content
-      src = join(draftDir, item);
-      return readFile(src);
-    }).then(content => {
-      // Create post
-      Object.assign(data, yfmParse(String(content), {}));
-      data.content = data._content;
-      data._content = undefined;
+        // Read the content
+        src = join(draftDir, item);
+        return readFile(src);
+      })
+      .then(content => {
+        // Create post
+        Object.assign(data, yfmParse(String(content), {}));
+        data.content = data._content;
+        data._content = undefined;
 
-      return this.create(data, replace);
-    }).then(post => {
-      result.path = post.path;
-      result.content = post.content;
-      return unlink(src);
-    }).then(() => { // Remove the original draft file
-      if (!config.post_asset_folder) return;
+        return this.create(data, replace);
+      })
+      .then(post => {
+        result.path = post.path;
+        result.content = post.content;
+        return unlink(src);
+      })
+      .then(() => {
+        // Remove the original draft file
+        if (!config.post_asset_folder) return;
 
-      // Copy assets
-      const assetSrc = removeExtname(src);
-      const assetDest = removeExtname(result.path);
+        // Copy assets
+        const assetSrc = removeExtname(src);
+        const assetDest = removeExtname(result.path);
 
-      return exists(assetSrc).then(exist => {
-        if (!exist) return;
+        return exists(assetSrc).then(exist => {
+          if (!exist) return;
 
-        return copyDir(assetSrc, assetDest).then(() => rmdir(assetSrc));
-      });
-    }).thenReturn(result).asCallback(callback);
+          return copyDir(assetSrc, assetDest).then(() => rmdir(assetSrc));
+        });
+      })
+      .thenReturn(result)
+      .asCallback(callback);
   }
 
   render(source, data: Data = {}, callback) {
@@ -394,76 +469,95 @@ class Post {
       // Read content from files
       promise = readFile(source);
     } else {
-      return Promise.reject(new Error('No input file or string!')).asCallback(callback);
+      return Promise.reject(new Error('No input file or string!')).asCallback(
+        callback
+      );
     }
 
     // Files like js and css are also processed by this function, but they do not require preprocessing like markdown
     // data.source does not exist when tag plugins call the markdown renderer
-    const isPost = !data.source || ['html', 'htm'].includes(ctx.render.getOutput(data.source));
+    const isPost
+      = !data.source
+      || ['html', 'htm'].includes(ctx.render.getOutput(data.source));
 
     if (!isPost) {
-      return promise.then(content => {
-        data.content = content;
-        ctx.log.debug('Rendering file: %s', magenta(source));
+      return promise
+        .then(content => {
+          data.content = content;
+          ctx.log.debug('Rendering file: %s', magenta(source));
 
-        return ctx.render.render({
-          text: data.content,
-          path: source,
-          engine: data.engine,
-          toString: true
-        });
-      }).then(content => {
-        data.content = content;
-        return data;
-      }).asCallback(callback);
+          return ctx.render.render({
+            text: data.content,
+            path: source,
+            engine: data.engine,
+            toString: true
+          });
+        })
+        .then((content: string) => {
+          data.content = content;
+          return data;
+        })
+        .asCallback(callback);
     }
 
     // disable Nunjucks when the renderer specify that.
-    let disableNunjucks = ext && ctx.render.renderer.get(ext) && !!ctx.render.renderer.get(ext).disableNunjucks;
+    let disableNunjucks
+      = ext
+      && ctx.render.renderer.get(ext)
+      && !!ctx.render.renderer.get(ext).disableNunjucks;
 
     // front-matter overrides renderer's option
-    if (typeof data.disableNunjucks === 'boolean') disableNunjucks = data.disableNunjucks;
+    if (typeof data.disableNunjucks === 'boolean') {
+      disableNunjucks = data.disableNunjucks;
+    }
 
     const cacheObj = new PostRenderEscape();
 
-    return promise.then(content => {
-      data.content = content;
-      // Run "before_post_render" filters
-      return ctx.execFilter('before_post_render', data, { context: ctx });
-    }).then(() => {
-      data.content = cacheObj.escapeCodeBlocks(data.content);
-      // Escape all Nunjucks/Swig tags
-      if (disableNunjucks === false) {
-        data.content = cacheObj.escapeAllSwigTags(data.content);
-      }
-
-      const options: { highlight?: boolean; } = data.markdown || {};
-      if (!config.syntax_highlighter) options.highlight = null;
-
-      ctx.log.debug('Rendering post: %s', magenta(source));
-      // Render with markdown or other renderer
-      return ctx.render.render({
-        text: data.content,
-        path: source,
-        engine: data.engine,
-        toString: true,
-        onRenderEnd(content) {
-          // Replace cache data with real contents
-          data.content = cacheObj.restoreAllSwigTags(content);
-
-          // Return content after replace the placeholders
-          if (disableNunjucks) return data.content;
-
-          // Render with Nunjucks
-          return tag.render(data.content, data);
+    return promise
+      .then((content: string) => {
+        data.content = content;
+        // Run "before_post_render" filters
+        return ctx.execFilter('before_post_render', data, { context: ctx });
+      })
+      .then(() => {
+        data.content = cacheObj.escapeCodeBlocks(data.content);
+        // Escape all Nunjucks/Swig tags
+        if (disableNunjucks === false) {
+          data.content = cacheObj.escapeAllSwigTags(data.content);
         }
-      }, options);
-    }).then(content => {
-      data.content = cacheObj.restoreCodeBlocks(content);
 
-      // Run "after_post_render" filters
-      return ctx.execFilter('after_post_render', data, { context: ctx });
-    }).asCallback(callback);
+        const options: { highlight?: boolean } = data.markdown || {};
+        if (!config.syntax_highlighter) options.highlight = null;
+
+        ctx.log.debug('Rendering post: %s', magenta(source));
+        // Render with markdown or other renderer
+        return ctx.render.render(
+          {
+            text: data.content,
+            path: source,
+            engine: data.engine,
+            toString: true,
+            onRenderEnd(content) {
+              // Replace cache data with real contents
+              data.content = cacheObj.restoreAllSwigTags(content);
+
+              // Return content after replace the placeholders
+              if (disableNunjucks) return data.content;
+
+              // Render with Nunjucks
+              return tag.render(data.content, data);
+            }
+          },
+          options
+        );
+      })
+      .then((content: any) => {
+        data.content = cacheObj.restoreCodeBlocks(content);
+
+        // Run "after_post_render" filters
+        return ctx.execFilter('after_post_render', data, { context: ctx });
+      })
+      .asCallback(callback);
   }
 }
 
