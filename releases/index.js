2391af38ac9532a7ade3710c55c96fbc46d97b3f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-28 23:04:48 +0700

    feat: add resolutions

--- releases/index.js
+++ releases/index.js
@@ -7,7 +7,7 @@ const tarballs = readdirSync(__dirname).filter((str) => str.endsWith('.tgz'));
 for (let i = 0; i < tarballs.length; i++) {
   const tgz = tarballs[i];
   const filename = basename(tgz, '.tgz');
-  pkg.dependencies[filename] = `file:./${tgz}`;
+  pkg.dependencies[filename] = pkg.resolutions[filename] = `file:./${tgz}`;
 }
 
 writeFileSync(join(__dirname, 'package.json'), JSON.stringify(pkg, null, 2) + '\n');
