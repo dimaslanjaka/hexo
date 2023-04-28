9739a5e9f87ce9ab5def66bc96e28d9ed845aaf3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-28 13:31:44 +0700

    refactor(script): update build script

--- packer.js
+++ packer.js
@@ -1,83 +1,70 @@
-const croSpawn = require("cross-spawn");
-const { renameSync, existsSync, rmSync, mkdirSync } = require("fs");
-const { join, dirname } = require("path");
+const croSpawn = require('cross-spawn');
+const { renameSync, existsSync, rmSync, mkdirSync, appendFileSync } = require('fs');
+const { join, dirname } = require('path');
 
 /**
  * @type {Promise<{location:string,name:string}[]>}
  */
 const parseWorkspaces = croSpawn
-	.async("yarn", ["workspaces", "list", "--no-private", "--json"], {
-		cwd: __dirname,
-	})
-	.then((o) =>
-		o.stdout
-			.split(/\r?\n/gm)
-			.filter((str) => str.length > 4)
-			.map((str) => {
-				const parse = JSON.parse(str.trim());
-				parse.location = join(__dirname, parse.location);
-				return parse;
-			})
-	);
+  .async('yarn', ['workspaces', 'list', '--no-private', '--json'], {
+    cwd: __dirname
+  })
+  .then((o) =>
+    o.stdout
+      .split(/\r?\n/gm)
+      .filter((str) => str.length > 4)
+      .map((str) => {
+        const parse = JSON.parse(str.trim());
+        parse.location = join(__dirname, parse.location);
+        return parse;
+      })
+  );
 
-parseWorkspaces
-	.then((workspaces) => {
-		const clean_build = (wname) =>
-			croSpawn
-				.async("yarn", ["workspace", wname, "run", "clean"], {
-					cwd: __dirname,
-				})
-				.then(() =>
-					croSpawn.async("yarn", ["workspace", wname, "run", "build"], {
-						cwd: __dirname,
-					})
-				);
-		return clean_build("warehouse")
-			.then(() => clean_build("hexo-front-matter"))
-			.then(() => clean_build("hexo-asset-link"))
-			.then(() => clean_build("hexo-log"))
-			.then(() =>
-				croSpawn.async(
-					"yarn",
-					["workspaces", "foreach", "--no-private", "run", "build"],
-					{ cwd: __dirname }
-				)
-			)
-			.then(() =>
-				croSpawn.async(
-					"yarn",
-					["workspaces", "foreach", "--no-private", "pack"],
-					{ cwd: __dirname }
-				)
-			)
-			.then(() => workspaces);
-	})
-	.then((workspaces) => {
-		const _run = () => {
-			for (let i = 0; i < workspaces.length; i++) {
-				const workspace = workspaces[i];
-				const tarballName = workspace.name + ".tgz";
-				const tarballPath = join(workspace.location, tarballName);
+const logfile = (...args) => appendFileSync(join(__dirname, 'build.log'), args.join('\n') + '\n');
 
-				// rename package.tgz to {workspace.name}.tgz
-				croSpawn.sync("yarn", [
-					"workspace",
-					workspace.name,
-					"exec",
-					`"mv package.tgz ${tarballName}"`,
-				]);
+parseWorkspaces.then((workspaces) => {
+  const clean_build = (wname) =>
+    croSpawn
+      .async('yarn', ['workspace', wname, 'run', 'clean'], {
+        cwd: __dirname
+      })
+      .then(() =>
+        croSpawn.async('yarn', ['workspace', wname, 'run', 'build'], {
+          cwd: __dirname
+        })
+      )
+      .then(() =>
+        croSpawn.async('yarn', ['workspace', wname, 'pack'], {
+          cwd: __dirname
+        })
+      )
+      .then(() => {
+        const workspace = workspaces.filter((o) => o.name === wname)[0];
+        if (typeof workspace === 'object') {
+          const tarballName = workspace.name + '.tgz';
+          const tarballPath = join(workspace.location, tarballName);
+          const originalTarballPath = join(workspace.location, 'package.tgz');
 
-				if (!existsSync(tarballPath)) {
-					console.log("fail packing " + tarballPath + " not found");
-					continue;
-				}
-
-				const dest = join(__dirname, "releases", tarballName);
-				if (!existsSync(dirname(dest))) mkdirSync(dirname(dest));
-				if (existsSync(dest)) rmSync(dest);
-				renameSync(tarballPath, dest);
-			}
-		};
-
-		_run();
-	});
+          // rename package.tgz to {workspace.name}.tgz
+          if (existsSync(originalTarballPath)) renameSync(originalTarballPath, tarballPath);
+          // move {workspace.name}.tgz to releases/{workspace.name}.tgz
+          if (existsSync(tarballPath)) {
+            const dest = join(__dirname, 'releases', tarballName);
+            if (!existsSync(dirname(dest))) mkdirSync(dirname(dest));
+            if (existsSync(dest)) rmSync(dest);
+            renameSync(tarballPath, dest);
+          } else {
+            logfile(tarballPath + ' not found');
+          }
+        } else {
+          logfile(wname, 'is not workspace');
+        }
+      })
+      .then(() => console.log(wname, 'clean->build->pack successful'));
+  return clean_build('warehouse')
+    .then(() => clean_build('hexo-front-matter'))
+    .then(() => clean_build('hexo-asset-link'))
+    .then(() => clean_build('hexo-log'))
+    .then(() => clean_build('hexo'))
+    .then(() => workspaces);
+});
