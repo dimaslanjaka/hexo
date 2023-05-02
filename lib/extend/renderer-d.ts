31d6a49f0f1ccc6b983dbb329e36fff14d8a3cc2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-02 17:58:16 +0700

    feat(data): export `StoreFunctionData` feat(data): `StoreFunctionData` is dynamic object
    prevent conflict type casting.

--- lib/extend/renderer-d.ts
+++ lib/extend/renderer-d.ts
@@ -1,18 +1,22 @@
+/**
+ * to cast `data` type look example below
+ * @example
+ * type rendererData = Parameters<Parameters<typeof hexo.extend.renderer.register>[2]>[0];
+ * // or
+ * type rendererData = Parameters<Parameters<import('hexo')['extend']['renderer']['register']>[2]>[0];
+ * // or
+ * type rendererData = import('hexo/extend/renderer-d.ts').StoreFunctionData;
+ */
+export interface StoreFunctionData {
+  [key: string]: any;
+  path?: string;
+  text: string;
+}
+
 export interface StoreSyncFunction {
   (
-
-    /**
-     * to cast `data` type look example below
-     * @example
-     * type rendererData = Parameters<Parameters<typeof hexo.extend.renderer.register>[2]>[0];
-     * // or
-     * type rendererData = Parameters<Parameters<import('hexo')['extend']['renderer']['register']>[2]>[0];
-     */
-    data: {
-      path?: string;
-      text: string;
-    },
-    options: Record<string, any>,
+    data: StoreFunctionData,
+    options: Record<string, any>
     // callback: (err: Error, value: string) => any
   ): any;
   output?: string;
@@ -21,21 +25,8 @@ export interface StoreSyncFunction {
 }
 
 export interface StoreFunction {
-  (
-    data: {
-      path?: string;
-      text: string;
-    },
-    options: Record<string, any>,
-  ): Promise<any>;
-  (
-    data: {
-      path?: string;
-      text: string;
-    },
-    options: Record<string, any>,
-    callback: (err: Error, value: string) => any
-  ): void;
+  (data: StoreFunctionData, options: Record<string, any>): Promise<any>;
+  (data: StoreFunctionData, options: Record<string, any>, callback: (err: Error, value: string) => any): void;
   output?: string;
   compile?: (local: Record<string, any>) => string;
   disableNunjucks?: boolean;
