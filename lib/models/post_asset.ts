b8049341066d1a954037f94b29b4bb40259a73aa
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-02 12:46:07 +0700

    chore: update from hexojs/hexo/pull/5153
    PR https://github.com/hexojs/hexo/pull/5153
    
    Commit https://github.com/hexojs/hexo/commit/8b95bbc722e5c77a7e8125441ed64d2ea3524ac0

--- lib/models/post_asset.ts
+++ lib/models/post_asset.ts
@@ -1,13 +1,13 @@
 import warehouse from 'warehouse';
-import { join } from 'path';
+import { dirname, join } from 'path';
 
-export = ctx => {
+export = (ctx: import('../hexo')) => {
   const PostAsset = new warehouse.Schema({
-    _id: {type: String, required: true},
-    slug: {type: String, required: true},
-    modified: {type: Boolean, default: true},
-    post: {type: warehouse.Schema.Types.CUID, ref: 'Post'},
-    renderable: {type: Boolean, default: true}
+    _id: { type: String, required: true },
+    slug: { type: String, required: true },
+    modified: { type: Boolean, default: true },
+    post: { type: warehouse.Schema.Types.CUID, ref: 'Post' },
+    renderable: { type: Boolean, default: true }
   });
 
   PostAsset.virtual('path').get(function() {
@@ -17,8 +17,8 @@ export = ctx => {
 
     // PostAsset.path is file path relative to `public_dir`
     // no need to urlescape, #1562
-    // strip /\.html?$/ extensions on permalink, #2134
-    return join(post.path.replace(/\.html?$/, ''), this.slug);
+    // strip extensions better on permalink, #2134
+    return join(dirname(post.path), post.slug, this.slug);
   });
 
   PostAsset.virtual('source').get(function() {
