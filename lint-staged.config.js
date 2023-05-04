2840b6e2e6c22fc1847303fd44cefac0616bb759
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-04 19:56:23 +0700

    feat: format yaml

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,8 @@ const config = {
   '*.ts': ['npx prettier --write', 'eslint --fix'],
   '*.html': ['npx eslint --fix', 'prettier --write'],
   '*.scss': 'npx prettier --write',
-  '*.json': 'npx prettier --write'
+  '*.json': 'npx prettier --write',
+  '*.yml': 'npx prettier --write'
 };
 
 module.exports = config;
