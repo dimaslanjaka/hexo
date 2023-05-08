99bbeb75f3a9a713e854b20fc4954ee0edb25c4a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-08 16:20:49 +0700

    fix: cast parameter type

--- lib/plugins/helper/fragment_cache.ts
+++ lib/plugins/helper/fragment_cache.ts
@@ -1,13 +1,14 @@
-
 import { Cache } from 'hexo-util';
 
-export = ctx => {
+export = (ctx: import('../../hexo')) => {
   const cache = new Cache();
 
   // reset cache for watch mode
-  ctx.on('generateBefore', () => { cache.flush(); });
+  ctx.on('generateBefore', () => {
+    cache.flush();
+  });
 
-  return function fragmentCache(id, fn) {
+  return function fragmentCache(id: string, fn: (...args: any[]) => any) {
     if (this.cache) return cache.apply(id, fn);
 
     const result = fn();
