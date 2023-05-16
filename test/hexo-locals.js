1a1b0e4fe5fd9735dece0d9347cab25802ec2923
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-16 17:00:19 +0700

    refactor: define global hexo

--- test/hexo-locals.js
+++ test/hexo-locals.js
@@ -1,8 +1,10 @@
 const path = require('path');
-const Hexo = require('../packages/hexo');
+const Hexo = require('../packages/hexo/dist/hexo');
 
 const hexo = new Hexo(path.join(__dirname, 'hexo-theme-test'));
+global.hexo = hexo;
+
 hexo.init().then(() => {
-  const locals = hexo.locals;
+  const locals = hexo.locals; //.get('posts');
   console.log(locals);
 });
