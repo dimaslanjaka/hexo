33d2ce2b5a626bb30a7d609efb9d90635c815e91
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-17 15:56:44 +0700

    chore: `HexoLocalsData` is dynamic object

--- lib/hexo/locals-d.ts
+++ lib/hexo/locals-d.ts
@@ -1,6 +1,7 @@
 import { Config } from './index-d';
 export type HexoLocalsFunc = import('../hexo')['_generateLocals'];
 export type HexoLocalsData = {
+  [key: string]: any;
   page: {
     [key: string]: any;
     path: string;
