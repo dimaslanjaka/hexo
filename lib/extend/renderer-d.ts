30bf0a2729e56fd9da65e6fde18653d6fdde8f23
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-17 17:02:17 +0700

    chore(StoreSyncFunction.compile): add `typeof njkRenderer.compile` chore(StoreFunction.compile): add `typeof njkRenderer.compile`

--- lib/extend/renderer-d.ts
+++ lib/extend/renderer-d.ts
@@ -1,3 +1,5 @@
+import njkRenderer from '../plugins/renderer/nunjucks';
+
 /**
  * to cast `data` type look example below
  * @example
@@ -20,7 +22,7 @@ export interface StoreSyncFunction {
     // callback: (err: Error, value: string) => any
   ): any;
   output?: string;
-  compile?: (local: Record<string, any>) => string;
+  compile?: ((local: Record<string, any>) => string) | (typeof njkRenderer)['compile'];
   disableNunjucks?: boolean;
 }
 
@@ -28,7 +30,7 @@ export interface StoreFunction {
   (data: StoreFunctionData, options: Record<string, any>): Promise<any>;
   (data: StoreFunctionData, options: Record<string, any>, callback: (err: Error, value: string) => any): void;
   output?: string;
-  compile?: (local: Record<string, any>) => string;
+  compile?: ((local: Record<string, any>) => string) | (typeof njkRenderer)['compile'];
   disableNunjucks?: boolean;
 }
 
