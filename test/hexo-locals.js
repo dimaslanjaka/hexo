2a4eb184a638f38f247bd6a600e7229e9129ae9a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-04 14:50:44 +0700

    add new event listener

--- test/hexo-locals.js
+++ test/hexo-locals.js
@@ -4,6 +4,10 @@ const Hexo = require('../packages/hexo/dist/hexo');
 const hexo = new Hexo(path.join(__dirname, 'hexo-theme-test'));
 global.hexo = hexo;
 
+hexo.on('new', function (post) {
+  console.log(post)
+})
+
 hexo.init().then(() => {
   const locals = hexo.locals; //.get('posts');
   console.log(locals);
