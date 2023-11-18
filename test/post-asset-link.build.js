d0b74404d6a0fd0762dc02d36416b2507a1722cd
dimaslanjaka <dimaslanjaka@gmail.com>
2023-11-18 10:43:31 +0700

    `eslint --fix test`

--- test/post-asset-link.build.js
+++ test/post-asset-link.build.js
@@ -1,23 +1,17 @@
-process.cwd = () => __dirname + "/hexo-theme-test";
+process.cwd = () => __dirname + '/hexo-theme-test';
 
-const Hexo = require("../packages/hexo/dist/hexo");
+const Hexo = require('../packages/hexo/dist/hexo');
 
 const hexo = new Hexo(process.cwd(), { debug: true });
 hexo.init().then(function () {
-	hexo.load().then(function () {
-		hexo.call("clean", {}, function () {
-			hexo.post
-				.render("source/_posts/post-asset-link.md")
-				.then(function (rendered) {
-					console.log(rendered.content);
-				});
-			hexo.post.render(
-				"source/_posts/post-asset-link.md",
-				{},
-				function (err, rendered) {
-					console.log(rendered.content);
-				}
-			);
-		});
-	});
+  hexo.load().then(function () {
+    hexo.call('clean', {}, function () {
+      hexo.post.render('source/_posts/post-asset-link.md').then(function (rendered) {
+        console.log(rendered.content);
+      });
+      hexo.post.render('source/_posts/post-asset-link.md', {}, function (err, rendered) {
+        console.log(rendered.content);
+      });
+    });
+  });
 });
