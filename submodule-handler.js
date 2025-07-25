a16ac2b37747ba9399411e648a609db330499016
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-07-25 17:54:26 +0700

    feat: enhance prepare script to include submodule handler for upstream management

--- submodule-handler.js
+++ submodule-handler.js
@@ -1,5 +1,7 @@
-const dotenv = require('dotenv');
-dotenv().config({});
+const { spawnSync } = require('child_process');
+const pc = require('picocolors');
+
+require('dotenv/config');
 
 const config = {
   'submodule "packages/hexo"': {
@@ -48,6 +50,45 @@ const config = {
   'submodule "packages/hexo-cli"': {
     path: 'packages/hexo-cli',
     origin: 'https://github.com/dimaslanjaka/hexo-cli.git',
-    branch: 'monorepo'
+    branch: 'monorepo',
+    upstream: 'https://github.com/hexojs/hexo-cli'
   }
 };
+
+for (const key in config) {
+  const submodule = config[key];
+  if (submodule.upstream) {
+    const remotes = spawnSync('git', ['remote', '-v'], { cwd: submodule.path, encoding: 'utf-8' });
+    if (remotes.error) {
+      console.error(pc.red(`Error fetching remotes for ${submodule.path}:`), remotes.error);
+      continue;
+    }
+    const parseRemotes = remotes.stdout
+      .split(/\r?\n/)
+      .filter((line) => line.trim() !== '')
+      .map((line) => {
+        return {
+          name: line.split(/\t/)[0],
+          url: line.split(/\t/)[1]?.split(' ')[0]
+        };
+      })
+      .filter((remote, index, self) => index === self.findIndex((r) => r.name === remote.name && r.url === remote.url));
+    const hasUpstream = parseRemotes.some((remote) => remote.name === 'upstream');
+    if (!hasUpstream) {
+      console.log(pc.yellow(`Adding upstream remote for ${submodule.path}`));
+      const addRemote = spawnSync('git', ['remote', 'add', 'upstream', submodule.upstream], { cwd: submodule.path });
+      if (addRemote.error) {
+        console.error(pc.red(`Error adding upstream remote for ${submodule.path}:`), addRemote.error);
+      } else {
+        console.log(pc.green(`Upstream remote added for ${submodule.path}`));
+      }
+    } else {
+      console.log(`Upstream remote already exists for ${pc.cyan(submodule.path)}`);
+      console.log(pc.bold(`Remotes for ${submodule.path}:`));
+      parseRemotes.forEach((remote) => {
+        const color = remote.name === 'upstream' ? pc.magenta : pc.blue;
+        console.log(`  ${color(remote.name)}: ${pc.dim(remote.url)}`);
+      });
+    }
+  }
+}
